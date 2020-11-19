9c30771f1588712230e1ae5657ecfa80161948e3
Taiba-k <73065414+Taiba-k@users.noreply.github.com>
2020-11-19 14:39:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+const Engine=Matter.Engine
+const World=Matter.World 
+const Bodies=Matter.Bodies
+
+var engine
+function setup() {
+  createCanvas(400,400);
+
+  var options={
+    isStatic:true
+  }
+ engine=Engine.create();
+ world=engine.world;
+ object= Bodies.rectangle(200,100,50,50,options)
+ World.add(world,object)
+ console.log(object)
+ console.log(object.position.y)
+}
+
+function draw() {
+  background(0);  
+  Engine.update(engine)
+  rectMode(CENTER)
+  rect(object.position.x, object.position.y, 50, 50);
+  
+}
